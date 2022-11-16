5e14545f6793f8fe15bb0c647d39ae6eedc6509b
Jacob-Altizer <jea25870@email.vccs.edu>
2022-11-16 11:25:18 -0500

    switched to SQLite3 for data storage

--- NRCCWebScraper/scraper.py
+++ NRCCWebScraper/scraper.py
@@ -3,51 +3,63 @@ import re
 import requests
 from bs4 import BeautifulSoup
 from helium import *
-import json
-
-
-def write_json(new_data, filename='data.json'):
-    with open(filename, 'r+') as file:
-        file_data = json.load(file)
-        file_data["Full-Stack Web Developer"].append(new_data)
-        file.seek(0)
-        json.dump(file_data, file, indent = 4)
+import os
+from pathlib import Path
+import sqlite3
 
 # Full stack url https://catalog.nr.edu/preview_program.php?catoid=30&poid=1282
 # NRCC programs list https://catalog.nr.edu/content.php?catoid=30&navoid=2019
 
+NRCC_Catalog = 'https://catalog.nr.edu/content.php?catoid=30&navoid=2019'
+
+
+connection = sqlite3.connect(Path(__file__).with_name('data.db'))
+cursor = connection.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS aas_program_index
+                        (program_name text, poid integer)''')
+
+
+cursor.execute('''INSERT INTO aas_program_index VALUES
+                        ('Information_Technology-Full_Stack_Developer_Specialization_AAS', '1282')''')
+
+connection.commit()
+
+for row in cursor.execute('''SELECT * FROM aas_program_index'''):
+    print(row)
+
 # opens a headless browser and goes to desired url / headless cuz javascript and such
-url = 'https://catalog.nr.edu/content.php?catoid=30&navoid=2019'
-browser = start_chrome(url, headless=True)
+def headless_browser(url):
 
-# scans html provided by browser var
-soup = BeautifulSoup(browser.page_source, 'html.parser')
+    browser = start_chrome(url, headless=True)
 
-# finds the Associate of Applied Science ul and returns every anchor tag
-AAS_programs_list = soup.select(selector='td.block_content ul:nth-of-type(2) a')
-
-program_name = []
-poid = []
-
-for program in AAS_programs_list:
-
-    program_name.append(program.text)
-
-    split_href = re.split('&|=', program.get('href'))
-    poid.append(split_href[-3])
-
-    # print(program.text, program.get('href')) # returns text from inside anchor tag, href from anchor tag
-
-AAS_programs = {program_name:poid for (program_name, poid) in zip(program_name, poid)}
-print(AAS_programs)
+    return browser
 
 
-# class_reqs = soup.find_all('li', class_='acalog-course')
 
+def program_address():
 
-# for req in class_reqs:
-#     data = {}
-#     data['required_class'] = req
+    program_name = []
+    poid = []
 
-# write_json(data)
+    # scans html provided by browser var
+    soup = BeautifulSoup(headless_browser(NRCC_Catalog).page_source, 'html.parser')
 
+    # finds the Associate of Applied Science ul and returns every anchor tag
+    AAS_programs_list = soup.select(selector='td.block_content ul:nth-of-type(2) a')
+
+    for program in AAS_programs_list:
+
+        # pulls text inside list anchor tags / replaces space chars with _
+        program_name.append(program.text.replace(' ', '_'))
+
+        # pulls poid value in list anchor tag hrefs
+        split_href = re.split('&|=', program.get('href'))
+        poid.append(int(split_href[-3]))
+
+    # zips program_name & poid lists into AAS_programs dictionary
+    AAS_programs = {program_name:poid for (program_name, poid) in zip(program_name, poid)}
+
+    return AAS_programs
+
+# print(program_address())
