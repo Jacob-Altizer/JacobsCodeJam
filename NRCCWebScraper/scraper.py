fa72af138bc63433eeb13473d223ed4d35e96dee
Jacob-Altizer <jea25870@email.vccs.edu>
2022-11-16 22:25:36 -0500

    created dynamic SQlite3 write to database function, scraper now finds all programs in catalog instead of just AAS

--- NRCCWebScraper/scraper.py
+++ NRCCWebScraper/scraper.py
@@ -1,6 +1,5 @@
 
 import re
-import requests
 from bs4 import BeautifulSoup
 from helium import *
 import os
@@ -13,21 +12,6 @@ import sqlite3
 NRCC_Catalog = 'https://catalog.nr.edu/content.php?catoid=30&navoid=2019'
 
 
-connection = sqlite3.connect(Path(__file__).with_name('data.db'))
-cursor = connection.cursor()
-
-cursor.execute('''CREATE TABLE IF NOT EXISTS aas_program_index
-                        (program_name text, poid integer)''')
-
-
-cursor.execute('''INSERT INTO aas_program_index VALUES
-                        ('Information_Technology-Full_Stack_Developer_Specialization_AAS', '1282')''')
-
-connection.commit()
-
-for row in cursor.execute('''SELECT * FROM aas_program_index'''):
-    print(row)
-
 # opens a headless browser and goes to desired url / headless cuz javascript and such
 def headless_browser(url):
 
@@ -37,29 +21,71 @@ def headless_browser(url):
 
 
 
-def program_address():
+def write_database(table_name, data, contents, data_format):
+
+    '''
+    Define formatting for table creating inside scraping function, return formatting vars as a list in order of write_database parameters\n
+    Example:
+
+        table_name = 'programs_index'
+        data = list(zip(program_name, poid))
+        contents = 'program_name TEXT, poid INTEGER'
+        data_format = '?,?'
+
+    table_name: name of the desired table to create/write to
+    data: list of tuples to be written
+    contents: desired column name(s) and their data type as tuple "(data_name1 TEXT, data_name2 INTEGER, etc...)"
+    data_format: column format to be created for table "(?,?,?,etc...)"
+    '''
+
+    connection = sqlite3.connect(Path(__file__).with_name('data.db'))
+    cursor = connection.cursor()
+
+    cursor.execute(f'''CREATE TABLE IF NOT EXISTS {table_name} ({contents})''')
+
+    cursor.executemany(f'''INSERT INTO {table_name} VALUES ({data_format})''', data)
+
+    connection.commit()
+
+    connection.close()
+
+
+
+def program_addresses():
 
     program_name = []
     poid = []
+    program_tag = []
 
     # scans html provided by browser var
     soup = BeautifulSoup(headless_browser(NRCC_Catalog).page_source, 'html.parser')
 
-    # finds the Associate of Applied Science ul and returns every anchor tag
-    AAS_programs_list = soup.select(selector='td.block_content ul:nth-of-type(2) a')
+    # finds each ul in NRCC Catalog
+    programs_list = soup.select(selector='td.block_content ul a')
 
-    for program in AAS_programs_list:
+    for program in programs_list:
 
         # pulls text inside list anchor tags / replaces space chars with _
         program_name.append(program.text.replace(' ', '_'))
+
+        # # pulls program type from text that has it
+        # if re.search('-', program) == True:
+        #     program_name_split = re.split('-', program.text)
+        #     program_tag.append(program_name_split[0])
 
         # pulls poid value in list anchor tag hrefs
         split_href = re.split('&|=', program.get('href'))
         poid.append(int(split_href[-3]))
 
-    # zips program_name & poid lists into AAS_programs dictionary
-    AAS_programs = {program_name:poid for (program_name, poid) in zip(program_name, poid)}
+    ## desired formatting for program_addresses function
 
-    return AAS_programs
+    table_name = 'programs_index' # name of table for this function
+    data = list(zip(program_name, poid)) # zips program_name & poid lists into AAS_programs list of tuples
+    contents = 'program_name TEXT, poid INTEGER' # column names and their value type
+    data_format = '?,?' # place holder column format
 
-# print(program_address())
+    return table_name, data, contents, data_format
+
+program_index = program_addresses()
+
+write_database(program_index[0], program_index[1], program_index[2], program_index[3])
